e04699678ae96886d4444195a3d7722b4e62ed5c
Yuxi Chen <yxchenyq@gmail.com>
2019-10-20 12:39:32 -0400

    The initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+
+#include <AMReX.H>
+#include <AMReX_Print.H>
+#include <AMReX_Box.H>
+#include <AMReX_REAL.H>
+#include <AMReX_IntVect.H>
+
+int main(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+
+
+    amrex::IntVect lo(AMREX_D_DECL(64,64,64));
+
+    amrex::AllPrint() << "Hello world from AMReX version " << amrex::Version() 
+		      <<" MyProc = "<< amrex::ParallelDescriptor::MyProc()  <<"\n";
+
+    
+
+    amrex::Print(1)<<"lo = "<<lo;
+
+    amrex::Finalize();
+}
+
