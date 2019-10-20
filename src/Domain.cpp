daf112db57c812ba648d9a98a2987eec5c7a4739
yuxichen <yuxichen@umich.edu>
2019-10-20 19:21:19 -0400

    Auto dependency.

---
+++ src/Domain.cpp
@@ -0,0 +1,7 @@
+#include "Domain.h"
+
+void Domain::init(){
+    domainPar.nCell[ix_] = 32;
+    domainPar.nCell[iy_] = 32;
+    domainPar.nCell[iz_] = 32;
+}
---
+++ src/include/Constants.h
@@ -0,0 +1,9 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+#include <AMReX.H>
+
+constexpr static int nDim = AMREX_SPACEDIM;
+constexpr static int ix_ = 0, iy_=1, iz_=2;
+
+#endif
