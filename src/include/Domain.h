996ef0dabc926ee6ecd56560939b73cb3c6b6e1a
yuxichen <yuxichen@umich.edu>
2019-10-20 13:36:10 -0400

    commit

---
+++ src/include/Domain.h
@@ -0,0 +1,18 @@
+#ifndef _DOMAIN_H_
+#define _DOMAIN_H_
+
+class Domain {
+  // public variables
+public:
+  // private variables
+private:
+  // public methods
+public:
+  Domain(){};
+  ~Domain(){};
+
+  // private methods
+private:
+};
+
+#endif
