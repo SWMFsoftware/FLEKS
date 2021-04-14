289ea7410cd126bf7035cfa02870e34e6043b934
Yuxi Chen <yuxichen@umich.edu>
2021-04-13 22:48:08 -0400

    Added methods to read, plot or save all the test particles at a given time.

--- tools/yt_interface.py
+++ tools/yt_interface.py
@@ -15,6 +15,7 @@ from yt.fields.field_info_container import FieldInfoContainer
 
 from utilities import plot_unit_planet, plot_unit_si, get_unit
 
+
 class FLEKSFieldInfo(FieldInfoContainer):
     l_units = "code_length"
     v_units = "code_velocity"
@@ -96,6 +97,7 @@ class FLEKSFieldInfo(FieldInfoContainer):
     def setup_particle_fields(self, ptype):
         super(FLEKSFieldInfo, self).setup_particle_fields(ptype)
 
+
 class FLEKSHierarchy(BoxlibHierarchy):
 
     def __init__(self, ds, dataset_type="boxlib_native"):
@@ -145,7 +147,7 @@ class FLEKSDataset(BoxlibDataset):
                  unit_system="mks"):
         self.default_fluid_type = "mesh"
         self.default_field = ("mesh", "density")
-        self.fluid_types = ("mesh", "index", "raw")                                
+        self.fluid_types = ("mesh", "index", "raw")
 
         super(FLEKSDataset, self).__init__(output_dir,
                                            cparam_filename,
@@ -192,10 +194,14 @@ class FLEKSDataset(BoxlibDataset):
         setdefaultattr(self, 'time_unit', self.quan(1, get_unit("time", unit)))
         setdefaultattr(self, 'length_unit', self.quan(1, get_unit("X", unit)))
         setdefaultattr(self, 'mass_unit', self.quan(1, get_unit("mass", unit)))
-        setdefaultattr(self, 'velocity_unit', self.quan(1, get_unit("u", unit)))
-        setdefaultattr(self, 'magnetic_unit', self.quan(1, get_unit("B", unit)))
-        setdefaultattr(self, 'density_unit', self.quan(1, get_unit("rho", unit)))
-        setdefaultattr(self, 'pressure_unit', self.quan(1, get_unit("p", unit)))
+        setdefaultattr(self, 'velocity_unit',
+                       self.quan(1, get_unit("u", unit)))
+        setdefaultattr(self, 'magnetic_unit',
+                       self.quan(1, get_unit("B", unit)))
+        setdefaultattr(self, 'density_unit',
+                       self.quan(1, get_unit("rho", unit)))
+        setdefaultattr(self, 'pressure_unit',
+                       self.quan(1, get_unit("p", unit)))
 
     def get_slice(self, norm, cut_loc):
         r""" 
@@ -208,7 +214,7 @@ class FLEKSDataset(BoxlibDataset):
         'x', 'y' or 'z' 
 
         cut_log: Float         
-        """ 
+        """
 
         axDir = {'X': 0, 'Y': 1, 'Z': 2}
         idir = axDir[norm.upper()]
@@ -248,7 +254,7 @@ class FLEKSDataset(BoxlibDataset):
     def get_domain(self):
         r"""
         Reading in all the simulation data into a 3D box. It returns a dataContainer3D object. 
-        """ 
+        """
         domain = self.covering_grid(
             level=0, left_edge=self.domain_left_edge, dims=self.domain_dimensions)
 
@@ -389,7 +395,7 @@ class FLEKSDataset(BoxlibDataset):
 
 class FLEKSTP(object):
     r"""
-    A class that is used to read and plot test particles.
+    A class that is used to read and plot test particles.    
 
     Parameters
     -----------
@@ -398,9 +404,12 @@ class FLEKSTP(object):
 
     Examples
     ----------
-    >>> tp = FLEKSTP("res/run1/PC/test_particles")
-    >>> pIDs = tp.IDs()
-    >>> tp.plot((2141,45))
+    >>> tp = FLEKSTP("res/run1/PC/test_particles", iSpecies=1)
+    >>> pIDs = list(tp.IDs())
+    >>> tp.plot_trajectory(pIDs[3])
+    >>> tp.save_trajectory_to_csv(pIDs[5])
+    >>> pData = tp.read_particles_at_time(6500.8, doSave=True)    
+    >>> f = tp.plot_loc(pData)
     """
 
     it_ = 0
@@ -415,8 +424,9 @@ class FLEKSTP(object):
         if type(outputDirs) == str:
             outputDirs = [outputDirs]
 
+        self.iSpecies = iSpecies
         self.plistfiles = list()
-        self.pfiles = list()        
+        self.pfiles = list()
         for outputDir in outputDirs:
             self.plistfiles = self.plistfiles+glob.glob(outputDir+"/FLEKS" +
                                                         str(iDomain)+"_particle_list_species_"+str(iSpecies)+"_*")
@@ -425,7 +435,7 @@ class FLEKSTP(object):
                                                   str(iDomain)+"_particle_species_"+str(iSpecies)+"_*")
 
         self.plistfiles.sort()
-        self.pfiles.sort()        
+        self.pfiles.sort()
 
         self.plists = []
         for fileName in self.plistfiles:
@@ -435,7 +445,13 @@ class FLEKSTP(object):
         for plist in self.plists:
             self.pset.update(plist.keys())
 
-        print('Particles of species ', iSpecies,' are read from ', outputDirs)
+        self.file_time = []
+        for filename in self.pfiles:
+            record = self._read_the_first_record(filename)
+            self.file_time.append(record[FLEKSTP.it_])
+
+        print('Particles of species ', self.iSpecies,
+              ' are read from ', outputDirs)
         print('Number of particles: ', len(self.pset))
 
     def read_particle_list(self, fileName):
@@ -454,8 +470,95 @@ class FLEKSTP(object):
                 plist.update({(cpu, id): loc})
         return plist
 
+    def _read_the_first_record(self, fileName):
+        r"""
+        Get the first record stored in one file. 
+        """
+        dataList = list()
+        unitSize = 7
+        with open(fileName, 'rb') as f:
+            while True:
+                binaryData = f.read(4*4)
+
+                if not binaryData:
+                    break  # EOF
+
+                (cpu, idtmp, nRecord, weight) = struct.unpack(
+                    'iiif', binaryData)
+                if nRecord > 0:
+                    binaryData = f.read(4*unitSize)
+                    dataList = dataList + \
+                        list(struct.unpack('f'*unitSize, binaryData))
+                    return dataList
+
+    def read_particles_at_time(self, time, doSave=False):
+        r"""
+        Get the information of all the particles at a given time, 
+        and save to a csv file with the name "particles_t***.csv" 
+        to the current directory if doSave is True. The location of 
+        these particles (the csv file) can be visualized with Paraview.
+
+        Examples
+        ----------
+        >>> pData = pt.read_particles_at_time(3700, doSave=True)
+        """
+
+        nFile = len(self.pfiles)
+        for iFile in range(nFile):
+            if iFile == 0 and time < self.file_time[iFile]:
+                raise Exception(
+                    "Error: There is no particle at the given time")
+
+            if iFile == nFile - 1:
+                break
+            if time > self.file_time[iFile] and time < self.file_time[iFile+1]:
+                break
+
+        fileName = self.pfiles[iFile]
+
+        unitSize = 7
+        dataList = []
+        with open(fileName, 'rb') as f:
+            while True:
+                binaryData = f.read(4*4)
+                if not binaryData:
+                    break  # EOF
+
+                (cpu, idtmp, nRecord, weight) = struct.unpack(
+                    'iiif', binaryData)
+                binaryData = f.read(4*unitSize*nRecord)
+                allRecords = list(struct.unpack(
+                    'f'*nRecord*unitSize, binaryData))
+                for i in range(nRecord):
+                    if(allRecords[unitSize*i + FLEKSTP.it_] > time or i == nRecord-1):
+                        dataList.append(allRecords[unitSize*i:unitSize*(i+1)])
+                        break
+
+        npData = np.array(dataList)
+
+        if doSave:
+            fileName = "particles_t"+str(time)+".csv"
+            np.savetxt(fileName, npData, delimiter=",",
+                       header="time, x, y, z, ux, uy, uz", comments="")
+
+        return npData
+
     def IDs(self):
         return self.pset
+
+    def save_trajectory_to_csv(self, partID, fileName=None):
+        r""" 
+        Save the trajectory of a particle to a csv file.  
+
+        Example
+        -----------------
+        >>> tp.save_trajectory_to_csv((3,15))
+        """
+        pData = self.read_particle_trajectory(partID)
+        if fileName == None:
+            fileName = "trajectory_"+str(partID[0])+"_"+str(partID[1])+".csv"
+        np.savetxt(fileName, pData, delimiter=",",
+                   header="time, x, y, z, ux, uy, uz", comments="")
 
     def read_particle_trajectory(self, partID):
         r"""
@@ -507,6 +610,21 @@ class FLEKSTP(object):
                 return dataList
 
     def select_particles(self, fSelect=None):
+        r"""
+        Select and return the particles that satisfy the requirement set by the 
+        user defined function fSelect. The first argument of fSelect is the particle
+        ID, and the second argument is the initial record (one record contains time,
+        location, velocity and weight of a particle) of a particle.
+
+        Examples
+        ----------
+        >>> def fselect(pid, pdata):
+        >>>     return pdata[FLEKSTP.it_] < 3601 and pdata[FLEKSTP.ix_] > 12 and abs(pdata[FLEKSTP.iy_])<1 and abs(pdata[FLEKSTP.iz_])<1
+
+        >>> pselected=tp.select_particles(fselect)  
+        >>> tp.plot_trajectory(list(pselected.keys())[1])
+        """
+
         selected = {}
         icount = 0
 
@@ -520,7 +638,17 @@ class FLEKSTP(object):
                 icount = icount + 1
         return selected
 
-    def plot_data(self, data):
+    def plot_trajectory(self, partID):
+        r""" 
+        Plots the trajectory and velocities of the particle partID. 
+
+        Example
+        -----------------
+        >>> tp.plot_trajectory((3,15))
+        """
+
+        data = self.read_particle_trajectory(partID)
+
         plt.ion()
         t = data[:, FLEKSTP.it_]
 
@@ -628,26 +756,47 @@ class FLEKSTP(object):
 
         return f
 
-    def plot(self, partID):
-        r""" 
-        Plots the trajectory and velocities of a particle. 
+    def plot_loc(self, pData):
+        r"""
+        Plot the location of particles pData
 
-        Example
-        -----------------
-        >>> tp.plot((3,15))
-        """        
-        pData = self.read_particle_trajectory(partID)
-        return self.plot_data(pData)
+        Examples
+        ----------
+        >>> pData = tp.read_particles_at_time(3700, doSave=True)
+        >>> f = tp.plot_loc(pData)
+        """
 
-    def save_to_csv(self, partID, fileName=None):
-        r""" 
-        Save the trajectory of a particle to a csv file.  
+        px = pData[:, FLEKSTP.ix_]
+        py = pData[:, FLEKSTP.iy_]
+        pz = pData[:, FLEKSTP.iz_]
 
-        Example
-        -----------------
-        >>> tp.save_to_csv((3,15))
-        """        
-        pData = self.read_particle_trajectory(partID)
-        if fileName == None:
-            fileName = "trajectory_"+str(partID[0])+"_"+str(partID[1])+".csv"
-        np.savetxt(fileName,pData,delimiter=",",header="time, x, y, z, ux, uy, uz",comments="")
+        f = plt.figure(figsize=(12, 12))
+
+        nrow = 2
+        ncol = 2
+        isub = 1
+        ax = f.add_subplot(nrow, ncol, isub)
+        ax.scatter(px, py, s=1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+
+        isub = 2
+        ax = f.add_subplot(nrow, ncol, isub)
+        ax.scatter(px, pz, s=1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('z')
+
+        isub = 3
+        ax = f.add_subplot(nrow, ncol, isub)
+        ax.scatter(py, pz, s=1)
+        ax.set_xlabel('y')
+        ax.set_ylabel('z')
+
+        isub = 4
+        ax = f.add_subplot(nrow, ncol, isub, projection='3d')
+        ax.scatter(px, py, pz, s=1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_ylabel('z')
+
+        return f
