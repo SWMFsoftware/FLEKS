3742c91b270e2d61c8c17ff5c404f89719fe9130
Yuxi Chen <yxchenyq@gmail.com>
2022-04-27 12:03:40 -0400

    Minor improvement

--- tools/fleks.py
+++ tools/fleks.py
@@ -1,20 +1,23 @@
-import glob, os
+import glob
+import os
 from idl_format_data import IDLDataSet
-from yt_interface import FLEKSDataset, FLEKSTP
+from yt_interface import FLEKSDataset
+from test_particles import FLEKSTP
 
-def load(filename):
+
+def load(filename, iDomain=0, iSpecies=0):
     files = glob.glob(filename)
     if len(files) == 0:
-        raise Exception('Error: can not find the file/director!')
+        raise Exception('Error: can not find the file/directory!')
     filename = files[0]
 
-    tmp = os.path.basename(os.path.normpath(filename))
-    
-    if tmp == 'test_particles':
-        return FLEKSTP(filename)
-    elif tmp.find('.') and tmp.split('.')[-1] in ['out', 'outs']:
+    basename = os.path.basename(os.path.normpath(filename))
+
+    if basename == 'test_particles':
+        return FLEKSTP(filename, iDomain=iDomain, iSpecies=iSpecies)
+    elif basename.find('.') and basename.split('.')[-1] in ['out', 'outs']:
         return IDLDataSet(filename)
-    elif tmp[-6:] == '_amrex':
+    elif basename[-6:] == '_amrex':
         return FLEKSDataset(filename)
     else:
         raise Exception('Error: unknown file format!')
