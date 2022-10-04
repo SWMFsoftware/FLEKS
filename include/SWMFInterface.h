341b1e96606edff0bbc14b6bb4a30f1fcf3525b8
Yuxi Chen <yuxichen@umich.edu>
2022-10-04 18:24:30 -0400

    Added OH_get_charge_exchange_wrapper() interface

---
+++ include/SWMFInterface.h
@@ -0,0 +1,26 @@
+#ifndef _SWMFINTERFACE_H_
+#define _SWMFINTERFACE_H_
+
+#include <mpi.h>
+#include <sstream>
+
+extern "C" {
+#ifdef _PT_COMPONENT_
+void OH_get_charge_exchange_wrapper(double *rhoIon, double *cs2Ion,
+                                    double uIon_D[3], double *rhoNeu,
+                                    double *cs2Neu, double uNeu_D[3],
+                                    double sourceIon_V[5],
+                                    double sourceNeu_V[5]);
+#endif
+}
+
+
+//   {
+//     double rhoIon, cs2Ion, uIon_D[3], rhoNeu, cs2Neu, uNeu_D[3], sourceIon_V[5],
+//         sourceNeu_V[5];
+//     OH_get_charge_exchange_wrapper(&rhoIon, &cs2Ion, uIon_D, &rhoNeu, &cs2Neu,
+//                                    uNeu_D, sourceIon_V, sourceNeu_V);
+//   }
+
+
+#endif
