cb8ff860e9dee0b0bf11c36b3c7c043e49a1338e
Yuxi Chen <yuxichen@umich.edu>
2022-10-24 22:54:17 -0400

    Added an empty OH_get_charge_exchange_wrapper() for PC compilation

--- include/SWMFInterface.h
+++ include/SWMFInterface.h
@@ -4,6 +4,7 @@
 #include <mpi.h>
 #include <sstream>
 
+#ifdef _PT_COMPONENT_
 extern "C" {
 void OH_get_charge_exchange_wrapper(double *rhoIon, double *cs2Ion,
                                     double uIon_D[3], double *rhoNeu,
@@ -11,14 +12,12 @@ void OH_get_charge_exchange_wrapper(double *rhoIon, double *cs2Ion,
                                     double sourceIon_V[5],
                                     double sourceNeu_V[5]);
 }
-
-
-//   {
-//     double rhoIon, cs2Ion, uIon_D[3], rhoNeu, cs2Neu, uNeu_D[3], sourceIon_V[5],
-//         sourceNeu_V[5];
-//     OH_get_charge_exchange_wrapper(&rhoIon, &cs2Ion, uIon_D, &rhoNeu, &cs2Neu,
-//                                    uNeu_D, sourceIon_V, sourceNeu_V);
-//   }
-
+#else
+void OH_get_charge_exchange_wrapper(double *rhoIon, double *cs2Ion,
+                                    double uIon_D[3], double *rhoNeu,
+                                    double *cs2Neu, double uNeu_D[3],
+                                    double sourceIon_V[5],
+                                    double sourceNeu_V[5]) {}
+#endif
 
 #endif
