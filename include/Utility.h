6ebe2fba9a0f0b2a00dcbb535af8125d1e0b9f33
Yuxi Chen <yuxichen@umich.edu>
2023-06-22 11:24:53 -0400

    Implemented print_fab().

--- include/Utility.h
+++ include/Utility.h
@@ -52,6 +52,39 @@ void print_MultiFab(const amrex::MultiFab& data, std::string tag,
 void print_MultiFab(const amrex::MultiFab& data, std::string tag,
                     amrex::Geometry& gm, int nshift = 0);
 
+template <class FAB>
+void print_fab(const amrex::FabArray<FAB>& fab, std::string tag,
+               const int iStart, const int nComp, int nshift = 0) {
+  amrex::AllPrint() << "-----" << tag << " begin-----" << std::endl;
+  amrex::Real sum = 0;
+  amrex::Real sum2 = 0;
+
+  for (amrex::MFIter mfi(fab); mfi.isValid(); ++mfi) {
+    const auto& fab = fab[mfi];
+    const auto& box = mfi.validbox();
+    const auto& data = fab.array();
+
+    const auto lo = amrex::lbound(box);
+    const auto hi = amrex::ubound(box);
+
+    amrex::AllPrint() << "------ box = " << box << std::endl;
+    for (int i = lo.x - nshift; i <= hi.x + nshift; ++i)
+      for (int j = lo.y - nshift; j <= hi.y + nshift; ++j)
+        for (int k = lo.z; k <= hi.z; ++k)
+          for (int iVar = iStart; iVar < iStart + nComp; iVar++) {
+            amrex::AllPrint() << " i = " << i << " j = " << j << " k = " << k
+                              << " iVar = " << iVar
+                              << " data = " << data(i, j, k, iVar) << std::endl;
+            sum += data(i, j, k, iVar);
+            sum2 += pow(data(i, j, k, iVar), 2);
+          }
+  }
+  amrex::AllPrint() << "sum = " << sum << " sum2 = " << sqrt(sum2)
+                    << " on proc = " << amrex::ParallelDescriptor::MyProc()
+                    << std::endl;
+  amrex::AllPrint() << "-----" << tag << " end-----" << std::endl;
+}
+
 // Return the firs integer in the string.
 // Example: "abc123def345b" -> 123
 inline int extract_int(std::string s) {
