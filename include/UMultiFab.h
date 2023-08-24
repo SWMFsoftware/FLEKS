9e59c52fa1700a53e058ccfec9f87b402ff28648
Talha Arshad <talhaa@umich.edu>
2023-08-24 15:18:11 -0400

    UMultiFab Interpolator defined
    The interpolator is not optimized. And will go through several changes.
    It is not yet clear whether the interpolator should be for class UMultiFab or for Array1D
    Secondly current implementation is slow because each Real component of Array1D<Real,243> is being accessed individually, not as a whole.
    This will be changed in the future.
    Committing because the basic structure of custom interpolators has been defined.

--- include/UMultiFab.h
+++ include/UMultiFab.h
@@ -1,6 +1,7 @@
 #ifndef _UMULTIFAB_H_
 #define _UMULTIFAB_H_
 
+#include "Array1D.h"
 #include <AMReX_BLassert.H>
 #include <AMReX_FArrayBox.H>
 #include <AMReX_FabArray.H>
@@ -8,6 +9,141 @@
 #include <AMReX_Periodicity.H>
 
 namespace amrex {
+
+class UMFInterp_NodeBilinear : public Interpolater {
+
+public:
+  // UMFInterp_NodeBilinear () {}
+  ~UMFInterp_NodeBilinear() override {}
+
+  Box CoarseBox(const Box& fine, int ratio) override {
+    Box b = amrex::coarsen(fine, ratio);
+
+    for (int i = 0; i < AMREX_SPACEDIM; i++) {
+      if (b.length(i) < 2) {
+        b.growHi(i, 1);
+      }
+    }
+
+    return b;
+  }
+
+  Box CoarseBox(const Box& fine, const IntVect& ratio) override {
+    Box b = amrex::coarsen(fine, ratio);
+
+    for (int i = 0; i < AMREX_SPACEDIM; i++) {
+      if (b.length(i) < 2) {
+        b.growHi(i, 1);
+      }
+    }
+
+    return b;
+  }
+
+  void interp(const FArrayBox& crse, int crse_comp, FArrayBox& fine,
+              int fine_comp, int ncomp, const Box& fine_region,
+              const IntVect& ratio, const Geometry& crse_geom,
+              const Geometry& fine_geom, Vector<BCRec> const& bcr,
+              int actual_comp, int actual_state, RunOn gpu_or_cpu) override {}
+
+  void interp(const BaseFab<RealMM>& crse, int crse_comp, BaseFab<RealMM>& fine,
+              int fine_comp, int ncomp, const Box& fine_region,
+              const IntVect& ratio, const Geometry& crse_geom,
+              const Geometry& fine_geom, Vector<BCRec> const& bcr,
+              int actual_comp, int actual_state, RunOn gpu_or_cpu) {
+    ncomp = 243;
+    Array4<RealMM const> const& crsearr = crse.const_array();
+    Array4<RealMM> const& finearr = fine.array();
+    RunOn runon = (gpu_or_cpu == RunOn::Gpu) ? RunOn::Gpu : RunOn::Cpu;
+    AMREX_HOST_DEVICE_PARALLEL_FOR_4D_FLAG(
+        runon, fine_region, ncomp, i, j, k, n, {
+          umf_nodebilin_interp(i, j, k, n, finearr, fine_comp, crsearr,
+                               crse_comp, ratio);
+        });
+  }
+
+  AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void umf_nodebilin_interp(
+      int i, int j, int k, int n, Array4<RealMM> const& fine, int fcomp,
+      Array4<RealMM const> const& crse, int ccomp,
+      IntVect const& ratio) noexcept {
+
+    int ic = amrex::coarsen(i, ratio[0]);
+    int jc = amrex::coarsen(j, ratio[1]);
+    int kc = amrex::coarsen(k, ratio[2]);
+    int ioff = i - ic * ratio[0];
+    int joff = j - jc * ratio[1];
+    int koff = k - kc * ratio[2];
+    Real rxinv = Real(1.0) / Real(ratio[0]);
+    Real ryinv = Real(1.0) / Real(ratio[1]);
+    Real rzinv = Real(1.0) / Real(ratio[2]);
+    if (ioff != 0 && joff != 0 && koff != 0) {
+      // Fine node at center of cell
+      fine(i, j, k).data[n + fcomp] =
+          rxinv * ryinv * rzinv *
+          (crse(ic, jc, kc).data[n + ccomp] * (ratio[0] - ioff) *
+               (ratio[1] - joff) * (ratio[2] - koff) +
+           crse(ic + 1, jc, kc).data[n + ccomp] * (ioff) * (ratio[1] - joff) *
+               (ratio[2] - koff) +
+           crse(ic, jc + 1, kc).data[n + ccomp] * (ratio[0] - ioff) * (joff) *
+               (ratio[2] - koff) +
+           crse(ic + 1, jc + 1, kc).data[n + ccomp] * (ioff) * (joff) *
+               (ratio[2] - koff) +
+           crse(ic, jc, kc + 1).data[n + ccomp] * (ratio[0] - ioff) *
+               (ratio[1] - joff) * (koff) +
+           crse(ic + 1, jc, kc + 1).data[n + ccomp] * (ioff) *
+               (ratio[1] - joff) * (koff) +
+           crse(ic, jc + 1, kc + 1).data[n + ccomp] * (ratio[0] - ioff) *
+               (joff) * (koff) +
+           crse(ic + 1, jc + 1, kc + 1).data[n + ccomp] * (ioff) * (joff) *
+               (koff));
+    } else if (joff != 0 && koff != 0) {
+      // Node on a Y-Z face
+      fine(i, j, k).data[n + fcomp] =
+          ryinv * rzinv *
+          (crse(ic, jc, kc).data[n + ccomp] * (ratio[1] - joff) *
+               (ratio[2] - koff) +
+           crse(ic, jc + 1, kc).data[n + ccomp] * (joff) * (ratio[2] - koff) +
+           crse(ic, jc, kc + 1).data[n + ccomp] * (ratio[1] - joff) * (koff) +
+           crse(ic, jc + 1, kc + 1).data[n + ccomp] * (joff) * (koff));
+    } else if (ioff != 0 && koff != 0) {
+      // Node on a Z-X face
+      fine(i, j, k).data[n + fcomp] =
+          rxinv * rzinv *
+          (crse(ic, jc, kc).data[n + ccomp] * (ratio[0] - ioff) *
+               (ratio[2] - koff) +
+           crse(ic + 1, jc, kc).data[n + ccomp] * (ioff) * (ratio[2] - koff) +
+           crse(ic, jc, kc + 1).data[n + ccomp] * (ratio[0] - ioff) * (koff) +
+           crse(ic + 1, jc, kc + 1).data[n + ccomp] * (ioff) * (koff));
+    } else if (ioff != 0 && joff != 0) {
+      // Node on a X-Y face
+      fine(i, j, k).data[n + fcomp] =
+          rxinv * ryinv *
+          (crse(ic, jc, kc).data[n + ccomp] * (ratio[0] - ioff) *
+               (ratio[1] - joff) +
+           crse(ic + 1, jc, kc).data[n + ccomp] * (ioff) * (ratio[1] - joff) +
+           crse(ic, jc + 1, kc).data[n + ccomp] * (ratio[0] - ioff) * (joff) +
+           crse(ic + 1, jc + 1, kc).data[n + ccomp] * (ioff) * (joff));
+    } else if (ioff != 0) {
+      // Node on X line
+      fine(i, j, k).data[n + fcomp] =
+          rxinv * ((ratio[0] - ioff) * crse(ic, jc, kc).data[n + ccomp] +
+                   (ioff)*crse(ic + 1, jc, kc).data[n + ccomp]);
+    } else if (joff != 0) {
+      // Node on Y line
+      fine(i, j, k).data[n + fcomp] =
+          ryinv * ((ratio[1] - joff) * crse(ic, jc, kc).data[n + ccomp] +
+                   (joff)*crse(ic, jc + 1, kc).data[n + ccomp]);
+    } else if (koff != 0) {
+      // Node on Z line
+      fine(i, j, k).data[n + fcomp] =
+          rzinv * ((ratio[2] - koff) * crse(ic, jc, kc).data[n + ccomp] +
+                   (koff)*crse(ic, jc, kc + 1).data[n + ccomp]);
+    } else {
+      // Node coincident with coarse node
+      fine(i, j, k).data[n + fcomp] = crse(ic, jc, kc).data[n + ccomp];
+    }
+  }
+};
 
 template <class T> class UMultiFab : public FabArray<BaseFab<T> > {
 public:
