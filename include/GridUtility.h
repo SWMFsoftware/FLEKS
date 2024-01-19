1347c8893027163f00bfdf03af397e0161f086be
Talha Arshad <talhaa@umich.edu>
2024-01-19 18:27:53 -0500

    Minor Addition

--- include/GridUtility.h
+++ include/GridUtility.h
@@ -142,6 +142,48 @@ void fill_fine_lev_bny_from_coarse(amrex::FabArray<FAB>& coarse,
   }
 }
 
+template <class FAB>
+void fill_fine_lev_from_coarse(amrex::FabArray<FAB>& coarse,
+                               amrex::FabArray<FAB>& fine, const int iStart,
+                               const int nComp, const amrex::IntVect ratio,
+                               const amrex::Geometry& cgeom,
+                               const amrex::Geometry& fgeom,
+                               const amrex::iMultiFab& fstatus,
+                               amrex::Interpolater& mapper) {
+  BL_PROFILE("fill_fine_lev_from_coarse");
+
+  amrex::FabArray<FAB> f(fine, amrex::make_alias, iStart, nComp);
+  amrex::FabArray<FAB> c(coarse, amrex::make_alias, iStart, nComp);
+
+  amrex::FabArray<FAB> ftmp(f.boxArray(), f.DistributionMap(), nComp,
+                            fine.nGrow());
+  ftmp.setVal(0.0);
+
+  interp_from_coarse_to_fine(c, ftmp, 0, nComp, ratio, cgeom, fgeom, &mapper,
+                             f.nGrow());
+
+  for (amrex::MFIter mfi(f); mfi.isValid(); ++mfi) {
+    FAB& fab = f[mfi];
+    const auto& box = mfi.fabbox();
+    const auto& data = fab.array();
+
+    const auto& statusArr = fstatus[mfi].array();
+    const auto& tmp = ftmp[mfi].array();
+
+    const auto lo = amrex::lbound(box);
+    const auto hi = amrex::ubound(box);
+
+    for (int iVar = 0; iVar < f.nComp(); iVar++)
+      for (int k = lo.z; k <= hi.z; ++k)
+        for (int j = lo.y; j <= hi.y; ++j)
+          for (int i = lo.x; i <= hi.x; ++i) {
+
+            data(i, j, k, iVar) = tmp(i, j, k, iVar);
+          }
+  }
+  fine.FillBoundary();
+}
+
 // Sum from coarse level to fine level for nodes at the boundary of two levels.
 template <class FAB>
 void sum_coarse_to_fine_lev_bny_node(
