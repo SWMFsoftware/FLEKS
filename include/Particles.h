eb8e633920d851eeaf9f69bab16e07cdb007138f
Talha Arshad <talhaa@umich.edu>
2024-10-17 06:11:05 -0400

    Format fix

--- include/Particles.h
+++ include/Particles.h
@@ -303,7 +303,8 @@ public:
                             amrex::Vector<amrex::MultiFab>& nodeBMF,
                             amrex::Vector<amrex::MultiFab>& u0MF,
                             amrex::Real dt, int iLev, bool solveInCoMov,
-                            amrex::Vector<amrex::iMultiFab>& nodestatus,amrex::Vector<amrex::iMultiFab>& cellstatus);
+                            amrex::Vector<amrex::iMultiFab>& nodestatus,
+                            amrex::Vector<amrex::iMultiFab>& cellstatus);
 
   void calc_jhat(amrex::MultiFab& jHat, amrex::MultiFab& nodeBMF,
                  amrex::Real dt);
