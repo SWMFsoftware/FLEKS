837d831f95a8d403b5472ca3d9bbf0a220eaebbf
Talha Arshad <talhaa@umich.edu>
2024-12-21 06:27:36 -0500

    Temporarily added optimized version of AMR calc_mass_matrix

--- include/Particles.h
+++ include/Particles.h
@@ -307,6 +307,15 @@ public:
                             amrex::Vector<amrex::iMultiFab>& nodestatus,
                             amrex::Vector<amrex::iMultiFab>& cellstatus);
 
+  void calc_mass_matrix_new_optimized(
+      amrex::Vector<amrex::UMultiFab<RealMM> >& nodeMM,
+      amrex::UMultiFab<RealMM>& nmmc, amrex::UMultiFab<RealMM>& nmmf,
+      amrex::Vector<amrex::MultiFab>& jHat, amrex::MultiFab& jhc,
+      amrex::MultiFab& jhf, amrex::Vector<amrex::MultiFab>& nodeBMF,
+      amrex::Vector<amrex::MultiFab>& u0MF, amrex::Real dt, int iLev,
+      bool solveInCoMov, amrex::Vector<amrex::iMultiFab>& nodestatus,
+      amrex::Vector<amrex::iMultiFab>& cellstatus);
+
   void calc_jhat(amrex::MultiFab& jHat, amrex::MultiFab& nodeBMF,
                  amrex::Real dt);
 
