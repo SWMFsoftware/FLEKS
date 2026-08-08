172b90cfc6c2983c55e7116ae19c0d4ec9a9be2d
hyzhou <hyzhou@umich.edu>
2026-08-08 07:06:40 -0400

    Fix folder structure generation.

--- src/main.cpp
+++ src/main.cpp
@@ -48,8 +48,8 @@ std::string prepare_standalone_run() {
   }
 
   if (amrex::ParallelDescriptor::IOProcessor()) {
-    std::filesystem::create_directories("FLEKS1/plots");
-    std::filesystem::create_directories("FLEKS1/restartOUT");
+    std::filesystem::create_directories("PC/plots");
+    std::filesystem::create_directories("PC/restartOUT");
   }
 
   return paramString;
