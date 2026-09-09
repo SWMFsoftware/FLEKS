a03765980ec22e24a9dbf620662ffda4997cfa13
Hongyang Zhou <hyzhou@umich.edu>
2026-09-09 09:05:21 -0400

    Refactor nodal interpolation using AMReX utilities
    - Use amrex::node_bilinear_interp for standard FArrayBox data in sum_coarse_to_fine_lev_bny_node
    - Delegate UInterp::CoarseBox to amrex::node_bilinear_interp.CoarseBox, removing redundant b.grow(1)
    - Remove unused UNode_FourthOrder class and node_fourth_order_interp member in Pic

--- include/GridUtility.h
+++ include/GridUtility.h
@@ -704,8 +704,13 @@ void sum_coarse_to_fine_lev_bny_node(
 
   amrex::FabArray<FAB> ftmp(f.boxArray(), f.DistributionMap(), nComp, 0);
   ftmp.setVal(0.0);
-  amrex::UNodeBilinear<typename FAB::value_type> mapper;
-  interp_from_coarse_to_fine(c, ftmp, 0, nComp, ratio, cgeom, fgeom, &mapper);
+  if constexpr (std::is_same_v<FAB, amrex::FArrayBox>) {
+    interp_from_coarse_to_fine(c, ftmp, 0, nComp, ratio, cgeom, fgeom,
+                               &amrex::node_bilinear_interp);
+  } else {
+    amrex::UNodeBilinear<typename FAB::value_type> mapper;
+    interp_from_coarse_to_fine(c, ftmp, 0, nComp, ratio, cgeom, fgeom, &mapper);
+  }
 
   for (amrex::MFIter mfi(f); mfi.isValid(); ++mfi) {
     FAB& fab = f[mfi];
